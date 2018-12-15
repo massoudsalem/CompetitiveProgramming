6f458a6363a4b41297d9d971e0976389eea60983
massoudsalem <massoudsalem@users.noreply.github.com>
2018-12-15 19:47:12 +0200

    Update

--- UVA/UVA10110.cpp
+++ UVA/UVA10110.cpp
@@ -7,18 +7,8 @@
 using namespace std;
 int main(int argc, char const *argv[]) { //Faster solution
   int n;
-  while(scanf("%d",&n),n){
+  while(scanf("%d",&n)&&n){
     int sqrtn=sqrt(n);
     printf("%s\n",(sqrtn*sqrtn==n)?"yes":"no");
   }
-  // while(scanf("%d",&n),n){ //this solution is also right but takes longer
-  //   int cnt=0;
-  //   for(int i=1;i*i<=n;++i){
-  //     if(n%i==0){
-  //       if(n/i==i)++cnt;a
-  //       else cnt+=2;
-  //     }
-  //   }
-  //   printf("%s\n",(cnt&1)?"yes":"no");
-  // }
 }
