b08e821a065cbecc78133324e1abdc6f51ea5135
Mohammad Massoud <36207910+massoudsalem@users.noreply.github.com>
2023-07-03 05:45:01 +0300

    LeetCode 125

---
+++ Leetcode/125.valid-palindrome.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=125 lang=javascript
+ *
+ * [125] Valid Palindrome
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+
+var isPalindrome = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right) {
+        if (!s[left].match(/[a-zA-Z0-9]/)) {
+            left++;
+            continue;
+        }
+        if (!s[right].match(/[a-zA-Z0-9]/)) {
+            right--;
+            continue;
+        }
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+    
+};
+// @lc code=end
