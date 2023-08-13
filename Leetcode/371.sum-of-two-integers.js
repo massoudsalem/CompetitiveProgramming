2be57d6cdd181f4a18e3beb1a14e923985a90e8a
Mohammad Massoud <36207910+massoudsalem@users.noreply.github.com>
2023-08-13 17:32:10 +0300

    LeetCode 371

---
+++ Leetcode/371.sum-of-two-integers.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=371 lang=javascript
+ *
+ * [371] Sum of Two Integers
+ */
+
+// @lc code=start
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+    return b === 0 ? a : getSum(a ^ b, (a & b) << 1);
+};
+// @lc code=end
+
