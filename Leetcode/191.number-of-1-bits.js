ff1708e749f96f6c0dff91c0a72e5d288a48e146
Mohammad Massoud <36207910+massoudsalem@users.noreply.github.com>
2023-08-13 16:59:35 +0300

    LeetCode 191

---
+++ Leetcode/191.number-of-1-bits.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=191 lang=javascript
+ *
+ * [191] Number of 1 Bits
+ */
+
+// @lc code=start
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let count = 0;
+    while(n != 0) {
+        count += n & 1;
+        n = n >>> 1;
+    }
+    return count;
+};
+// @lc code=end
+
